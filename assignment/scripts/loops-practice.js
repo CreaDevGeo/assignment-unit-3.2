abc3e9e1b2bef1704cbea28a087d09414445ca4c
Georgio Harris <georgie.to.harris@gmail.com>
2023-05-14 15:42:52 -0500

    Completed half of the loops tasks up until 3a.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,17 +13,32 @@ for (let i=0; i<4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+for (let i=0; i<= 5; i++) {  
+  console.log(i);
+}
+// The conditional statement of the larger number needed to increase from 4 to 5. Less than symbol changed to less than or equal to.
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+for(let i = 3; i <= 5; i++){
+  console.log(i);
+}
+// The value of the created i variable needed to increase from 0 to 3 to start the loop from there.
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+for(let i = 2; i <= 10; i+= 2){
+  console.log(i);
+}
+//The i variable needed to derease from 3 to 2. Conditional statement number increase fro 5 to 10.
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
+for(let i = 5; i >= 0; i --){
+  console.log(i);
+}
 
 
 // 2. For of loops  
@@ -32,13 +47,19 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for(let star of stars){
+  console.log(star);
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+star = 0
+while( star < stars.length){
+console.log(star[]);
+}
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
